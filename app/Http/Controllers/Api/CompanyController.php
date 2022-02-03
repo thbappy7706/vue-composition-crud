ade9f44f0cd2c4432482132153c74e5b2669a6ab
thbappy7706 <tanvir15-7706@diu.edu.bd>
2022-02-03 13:27:29 +0600

    resource added

---
+++ app/Http/Controllers/Api/CompanyController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CompanyResource;
+use App\Models\Company;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CompanyController extends Controller
+{
+
+    protected $companyModel;
+    public function __construct(Company $company)
+    {
+        $this->companyModel = $company;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    {
+        return CompanyResource::collection($this->companyModel->orderBy('id')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}
