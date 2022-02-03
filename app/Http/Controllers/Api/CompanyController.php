c304aa7c65d5a0500d92c2af39a63bcf3d36492a
thbappy7706 <tanvir15-7706@diu.edu.bd>
2022-02-03 13:33:14 +0600

    api controller

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CompanyRequest;
 use App\Http\Resources\CompanyResource;
 use App\Models\Company;
 use Illuminate\Http\Request;
@@ -11,10 +12,11 @@ use Illuminate\Http\Response;
 class CompanyController extends Controller
 {
 
-    protected $companyModel;
+    protected $model;
+
     public function __construct(Company $company)
     {
-        $this->companyModel = $company;
+        $this->model = $company;
     }
 
     /**
@@ -24,51 +26,54 @@ class CompanyController extends Controller
      */
     public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
-        return CompanyResource::collection($this->companyModel->orderBy('id')->get());
+        return CompanyResource::collection($this->model->orderBy('id')->get());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return CompanyResource
      */
-    public function store(Request $request)
+    public function store(CompanyRequest $request): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
-        //
+        $company = $this->model->create($request->validated());
+        return new CompanyResource($company);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\Company $company
+     * @return CompanyResource
      */
-    public function show(Company $company)
+    public function show(Company $company): CompanyResource
     {
-        //
+        return new CompanyResource($company);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Company  $company
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Company $company
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Company $company)
+    public function update(CompanyRequest $request, Company $company): CompanyResource
     {
-        //
+        $company->update($request->validated());
+        return new CompanyResource($company);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Company  $company
+     * @param \App\Models\Company $company
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Company $company)
+    public function destroy(Company $company): Response
     {
-        //
+        $company->delete();
+        return response()->noContent();
     }
 }
